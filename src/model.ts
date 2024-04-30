4c867c21e9a1ba5d0dd9a5d1ac433671d572b7ea
polukarp <perzhul.works@gmail.com>
2024-04-30 15:57:04 +0500

    chore: add .env variable for amount of results

--- src/model.ts
+++ src/model.ts
@@ -14,7 +14,8 @@ export const userRemoved = createEvent<User>();
 export const $users = createStore<User[]>([]);
 export const $filteredUsers = createStore<User[]>([]);
 
-const DEFAULT_USERS_COUNT = 500;
+const DEFAULT_USERS_COUNT = import.meta.env.VITE_RESULTS_COUNT || 500;
+
 export const $usersCount = createStore(DEFAULT_USERS_COUNT);
 export const $filteredUsersCount = $filteredUsers.map((users) => users.length);
 
